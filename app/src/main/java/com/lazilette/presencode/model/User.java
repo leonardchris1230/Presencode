f19b0a6f3ec7871740ce8ceb6f7aa8206b637d49
l.chrissan <l.chrissan@gmail.com>
2022-02-14 08:56:46 +0700

    Adding early register function

---
+++ app/src/main/java/com/lazilette/presencode/model/User.java
@@ -0,0 +1,29 @@
+package com.lazilette.presencode.model;
+
+public class User {
+    String uid, name, email;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- app/src/main/java/com/lazilette/presencode/ui/register/RegisterActivity.java
+++ app/src/main/java/com/lazilette/presencode/ui/register/RegisterActivity.java
@@ -1,19 +1,36 @@
 package com.lazilette.presencode.ui.register;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import com.lazilette.presencode.R;
 import com.lazilette.presencode.databinding.ActivityLoginBinding;
 import com.lazilette.presencode.databinding.ActivityRegisterBinding;
+import com.lazilette.presencode.model.User;
+import com.lazilette.presencode.ui.home.HomeActivity;
 import com.lazilette.presencode.ui.login.LoginActivity;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
     ActivityRegisterBinding binding;
+    FirebaseAuth auth;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +38,75 @@ public class RegisterActivity extends AppCompatActivity {
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        auth = FirebaseAuth.getInstance();
+
+
+        binding.imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uname = binding.registerUsername.getText().toString();
+                String email = binding.registerEmail.getText().toString();
+                String password = binding.registerPassword .getText().toString();
+
+                if (TextUtils.isEmpty(uname) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(RegisterActivity.this, "Mohon isi semua field", Toast.LENGTH_SHORT).show();
+                } else if (password.length() < 6) {
+                    Toast.makeText(RegisterActivity.this, "Password minimal terdiri dari 6 karakter", Toast.LENGTH_SHORT).show();
+                } else {
+                    register(uname, email, password);
+                }
+
+            }
+        });
+
+
+
         binding.registerMasuk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
             }
         });
+
+
+
+    }
+    private void register(String username, String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userid = firebaseUser.getUid();
+                            User u = new User();
+                            u.setName(username);
+                            u.setUid(userid);
+                            u.setEmail(email);
+                            reference = FirebaseDatabase.getInstance().getReference().child("Users").child(userid);
+
+                            HashMap<String, Object> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username", username.toLowerCase());
+                            hashMap.put("email", email);
+
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+
+                                        Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                        } else {
+
+                            Toast.makeText(RegisterActivity.this, "Maaf, kamu tidak bisa register dengan username atau email ini", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
